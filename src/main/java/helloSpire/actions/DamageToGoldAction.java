d06fb9bb91c8acbb6220e679c20efdf984dc2838
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-06-22 19:34:09 -0700

    Adds In-Game description and image for DamageToGoldStrike.

--- src/main/java/helloSpire/actions/DamageToGoldAction.java
+++ src/main/java/helloSpire/actions/DamageToGoldAction.java
@@ -4,43 +4,40 @@ package helloSpire.actions;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.ActionType;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.GainPennyEffect;
 import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
 public class DamageToGoldAction extends AbstractGameAction {
-    private int increaseGold;
     private DamageInfo info;
-    private static final float DURATION = 0.1F;
 
-    public DamageToGoldAction(AbstractCreature target, DamageInfo info, int goldAmount) {
+    public DamageToGoldAction(AbstractCreature target, DamageInfo info, AttackEffect effect) {
         this.info = info;
         this.setValues(target, info);
-        this.increaseGold = goldAmount;
         this.actionType = ActionType.DAMAGE;
-        this.duration = 0.1F;
+        this.attackEffect = effect;
     }
 
     public void update() {
-        if (this.duration == 0.1F && this.target != null) {
-            AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.BLUNT_HEAVY));
-            this.target.damage(this.info);
+        if (this.duration == 0.5F) {
+            AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, this.attackEffect));
+        }
 
-            // gain gold --------------------------------------------------
-            AbstractDungeon.player.gainGold(this.increaseGold);
-            for (int i = 0; i < this.increaseGold; ++i) {
-                AbstractDungeon.effectList.add(new GainPennyEffect(this.source, this.target.hb.cX, this.target.hb.cY, this.source.hb.cX, this.source.hb.cY, true));
+        this.tickDuration();
+        if (this.isDone) {
+            this.target.damage(this.info);
+            if (this.target.lastDamageTaken > 0) {
+                this.addToTop(new HealAction(this.source, this.source, this.target.lastDamageTaken));
+                this.addToTop(new WaitAction(0.1F));
             }
-            // gain gold --------------------------------------------------
 
             if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
                 AbstractDungeon.actionManager.clearPostCombatActions();
             }
         }
 
-        this.tickDuration();
     }
 }
--- src/main/java/helloSpire/cards/DamageToGoldStrike.java
+++ src/main/java/helloSpire/cards/DamageToGoldStrike.java
@@ -1,6 +1,6 @@
 package helloSpire.cards;
 
-
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 
@@ -10,6 +10,7 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import helloSpire.DefaultMod;
 import helloSpire.actions.DamageToGoldAction;
+import helloSpire.actions.FlatGoldAction;
 import helloSpire.characters.TheDefault;
 
 import static helloSpire.DefaultMod.makeCardPath;
@@ -26,24 +27,22 @@ public class DamageToGoldStrike extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 0;
-    private static final int DAMAGE = 1;
+    private static final int DAMAGE = 10;
     private static final int UPGRADE_PLUS_DMG = 1;
 
     public DamageToGoldStrike() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         this.baseDamage = DAMAGE;
-        this.baseMagicNumber = 20;
-        this.magicNumber = this.baseMagicNumber; //TODO: Modify Magic number
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DamageToGoldAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), this.magicNumber));
+        this.addToBot(new DamageToGoldAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AttackEffect.NONE));
     }
 
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(5);
+            this.upgradeDamage(UPGRADE_PLUS_DMG);
             this.upgradeMagicNumber(5);
         }
 
