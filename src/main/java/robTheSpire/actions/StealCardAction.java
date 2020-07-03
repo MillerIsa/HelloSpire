830d48be81b52d007813c8a349cc65c18345b32b
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-07-02 23:09:28 -0700

    Removes Default Cards from the card pool for StealCardAction.java

--- src/main/java/robTheSpire/actions/StealCardAction.java
+++ src/main/java/robTheSpire/actions/StealCardAction.java
@@ -8,22 +8,21 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.screens.CardRewardScreen;
+import robTheSpire.cards.*;
 import robTheSpire.characters.TheDefault;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StealCardAction extends AbstractGameAction {
     private boolean retrieveCard = false;
     private static final CardType CARD_TYPE = null;
-    final CardGroup anyCard;
+    private final CardGroup anyCard;
 
     public StealCardAction() {
         this.actionType = ActionType.CARD_MANIPULATION;
         this.duration = Settings.ACTION_DUR_FAST;
         this.amount = 1;
-        anyCard = getCardGroup();
+        anyCard = getCardGroup(getProhibitedCardIDs());
     }
 
 
@@ -31,7 +30,7 @@ public class StealCardAction extends AbstractGameAction {
         ArrayList<AbstractCard> generatedCards = this.generateCardChoices();
 
         if (this.duration == Settings.ACTION_DUR_FAST) {
-            AbstractDungeon.cardRewardScreen.customCombatOpen(generatedCards, CardRewardScreen.TEXT[1],true);
+            AbstractDungeon.cardRewardScreen.customCombatOpen(generatedCards, CardRewardScreen.TEXT[1], true);
         } else {
             if (!this.retrieveCard) {
                 if (AbstractDungeon.cardRewardScreen.discoveryCard != null) {
@@ -44,7 +43,7 @@ public class StealCardAction extends AbstractGameAction {
                     if (this.amount == 1) {
 
                         AbstractDungeon.player.masterDeck.addToTop(disCard);
-                    } else{
+                    } else {
                         throw new IllegalStateException("Tried to steal more than one card. This is invalid because it is only possible to steal one card.");
                     }
 
@@ -61,7 +60,7 @@ public class StealCardAction extends AbstractGameAction {
     private ArrayList<AbstractCard> generateCardChoices() {
         ArrayList<AbstractCard> derp = new ArrayList<>();
 
-        while(derp.size() != 3) {
+        while (derp.size() != 3) {
             boolean dupe = false;
             AbstractCard tmp;
             tmp = this.getAnyColorCard(AbstractDungeon.rollRarity());
@@ -81,25 +80,45 @@ public class StealCardAction extends AbstractGameAction {
         return derp;
     }
 
-    private static CardGroup getCardGroup(){
+    private static CardGroup getCardGroup(Set<String> prohibited) {
         CardGroup temp = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
-        for (final Map.Entry<String, AbstractCard> c : CardLibrary.cards.entrySet()) {
-            if (c.getValue().color != AbstractCard.CardColor.COLORLESS && c.getValue().type != AbstractCard.CardType.CURSE && c.getValue().type != AbstractCard.CardType.STATUS && c.getValue().color == TheDefault.Enums.COLOR_GRAY) {
-                temp.addToBottom(c.getValue());
+        for (final Map.Entry<String, AbstractCard> cardEntry : CardLibrary.cards.entrySet()) {
+            AbstractCard c = cardEntry.getValue();
+//            System.out.println("c's cardID" + c.cardID);
+//            System.out.println("prohibited.contains(c.cardID): " + prohibited.contains(c.cardID));
+            if (c.color != AbstractCard.CardColor.COLORLESS && c.type != AbstractCard.CardType.CURSE &&
+                    c.type != AbstractCard.CardType.STATUS && c.color == TheDefault.Enums.COLOR_GRAY &&
+                    !prohibited.contains(c.cardID)) {
+                temp.addToBottom(c);
             }
         }
+       System.out.println("Stealable Thief Cards: " + temp.getCardNames().toString());
         return temp;
     }
 
     //Returns a card based on the rarity roll passed to this method
     private AbstractCard getAnyColorCard(final AbstractCard.CardRarity rarity) {
         anyCard.shuffle(AbstractDungeon.cardRng);
-        return anyCard.getRandomCard(true, rarity);
+        return anyCard.getRandomCard(true/*, rarity*/);//TODO: reimpliment Rarity behavior
     }
 
     //TODO: add prohibited cards
-    private static List<AbstractCard> getProhibitedCards(){
-        ArrayList<AbstractCard> prohibited = new ArrayList<>();
-        return prohibited;
+    private static Set<String> getProhibitedCardIDs() {
+        Set<String> prohibitedIDs = new HashSet<>();
+        prohibitedIDs.add(DefaultAttackWithVariable.ID);
+        prohibitedIDs.add(DefaultCommonAttack.ID);
+        prohibitedIDs.add(DefaultCommonPower.ID);
+        prohibitedIDs.add(DefaultCommonSkill.ID);
+        prohibitedIDs.add(DefaultRareSkill.ID);
+        prohibitedIDs.add(DefaultRareAttack.ID);
+        prohibitedIDs.add(DefaultRarePower.ID);
+        prohibitedIDs.add(DefaultUncommonSkill.ID);
+        prohibitedIDs.add(DefaultUncommonAttack.ID);
+        prohibitedIDs.add(DefaultUncommonPower.ID);
+        prohibitedIDs.add(OrbSkill.ID);
+        prohibitedIDs.add(DefaultSecondMagicNumberSkill.ID);
+        System.out.println("Prohibited Cards " + prohibitedIDs.toString());
+        return prohibitedIDs;
     }
+
 }
