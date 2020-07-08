61f07d1e3b7c1b6c694761a5b5a07da56825f5a7
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-07-07 20:23:50 -0700

    Adds Kotlin to the project. Starts implementation of GoldenWallPower and GoldenWallPowerCard.

---
+++ src/main/java/robTheSpire/cards/GoldenWallPowerCard.java
@@ -0,0 +1,57 @@
+package robTheSpire.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import robTheSpire.DefaultMod;
+import robTheSpire.characters.TheDefault;
+import robTheSpire.powers.GoldenWallPower;
+
+import static robTheSpire.DefaultMod.makeCardPath;
+
+public class GoldenWallPowerCard extends AbstractDynamicCard{
+
+        public static final String ID = DefaultMod.makeID(GoldenWallPowerCard.class.getSimpleName());
+        public static final String IMG = makeCardPath("Power.png");
+
+
+        private static final CardRarity RARITY = CardRarity.UNCOMMON;
+        private static final CardTarget TARGET = CardTarget.SELF;
+        private static final CardType TYPE = CardType.POWER;
+        public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+        private static final int COST = 1;
+        private static final int UPGRADED_COST = 1;
+
+        private static final int DAMAGE = 0;
+        private static final int UPGRADE_PLUS_DMG = 0;
+
+        private static final int ARMOR = 5;
+
+
+        public GoldenWallPowerCard() {
+            super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+            baseDamage = DAMAGE;
+        }
+
+
+        // Actions the card should do.
+        @Override
+        public void use(AbstractPlayer p, AbstractMonster m) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new ApplyPowerAction(m, m, new GoldenWallPower(p, ARMOR), ARMOR));
+        }
+
+
+        // Upgraded stats.
+        @Override
+        public void upgrade() {
+            if (!upgraded) {
+                upgradeName();
+                upgradeDamage(UPGRADE_PLUS_DMG);
+                upgradeBaseCost(UPGRADED_COST);
+                initializeDescription();
+            }
+        }
+}
---
+++ src/main/java/robTheSpire/powers/GoldenWallPower.kt
@@ -0,0 +1,41 @@
+package robTheSpire.powers
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction
+import com.megacrit.cardcrawl.core.AbstractCreature
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.localization.PowerStrings
+import com.megacrit.cardcrawl.powers.AbstractPower
+import robTheSpire.DefaultMod
+
+
+class GoldenWallPower(owner: AbstractCreature, armorAmt: Int) : AbstractPower() {
+    override fun playApplyPowerSfx() {
+        CardCrawlGame.sound.play("POWER_METALLICIZE", 0.05f)
+    }
+
+    override fun updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + CONVERSION_RATE + DESCRIPTIONS[2]
+    }
+
+    override fun atEndOfTurnPreEndTurnCards(isPlayer: Boolean) {
+        flash()
+        addToBot(GainBlockAction(owner, owner, amount))
+    }
+
+    companion object {
+        const val CONVERSION_RATE = 2;
+        val POWER_ID: String = DefaultMod.makeID(Companion::class.java.enclosingClass.simpleName)
+        private var powerStrings: PowerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID)
+        var NAME: String = powerStrings.NAME
+        val DESCRIPTIONS: Array<String> = powerStrings.DESCRIPTIONS
+    }
+
+    init {
+        name = NAME
+        ID = POWER_ID
+        amount = armorAmt
+        updateDescription()
+        loadRegion("armor")
+    }
+}
+//TODO: Update Maven to copy the FAT jar instead of the normal jar.
