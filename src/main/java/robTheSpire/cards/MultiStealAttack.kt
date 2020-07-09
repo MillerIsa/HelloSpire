f34e3b166a7f252686c8cf8c7902f158be022ad4
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-07-09 08:25:36 -0700

    Changes Luck Amplifiers name to Lucky Charm. Also changes MultiStealAttack.kt's rarity to RARE.

--- src/main/java/robTheSpire/cards/MultiStealAttack.kt
+++ src/main/java/robTheSpire/cards/MultiStealAttack.kt
@@ -11,7 +11,7 @@ import robTheSpire.actions.MultiStealAction
 import robTheSpire.characters.TheDefault
 
 
-class MultiStealAttack : AbstractCard(ID, cardStrings.NAME, "red/attack/pummel", 1, cardStrings.DESCRIPTION, CardType.ATTACK, TheDefault.Enums.COLOR_GRAY, CardRarity.UNCOMMON, CardTarget.ENEMY) {
+class MultiStealAttack : AbstractCard(ID, cardStrings.NAME, "red/attack/pummel", 1, cardStrings.DESCRIPTION, CardType.ATTACK, TheDefault.Enums.COLOR_GRAY, CardRarity.RARE, CardTarget.ENEMY) {
     override fun use(p: AbstractPlayer, m: AbstractMonster) {
         for (i in 0 until magicNumber) {
             addToBot(MultiStealAction(m, DamageInfo(p, damage, damageTypeForTurn)))
