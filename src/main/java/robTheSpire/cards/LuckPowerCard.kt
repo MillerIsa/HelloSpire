0e0a3d0ae59f64911eb38712f1d78edc8c33f990
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-07-08 22:06:04 -0700

    Changes LuckPowerCard.kt so it always applies 1 stack of luck.

--- src/main/java/robTheSpire/cards/LuckPowerCard.kt
+++ src/main/java/robTheSpire/cards/LuckPowerCard.kt
@@ -12,13 +12,13 @@ import robTheSpire.powers.LuckPower
 
 class LuckPowerCard : AbstractDynamicCard(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET) {
     override fun use(p: AbstractPlayer, m: AbstractMonster?){
-        AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(p, p, LuckPower(p, LUCK_AMOUNT)))
+        AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(p, p, LuckPower(p, LUCK_AMOUNT), LUCK_AMOUNT))
     }
 
     companion object {
         @JvmField val ID: String = DefaultMod.makeID(Companion::class.java.enclosingClass.simpleName)
         private val cardStrings: CardStrings = CardCrawlGame.languagePack.getCardStrings(ID)
-        const val LUCK_AMOUNT = 2
+        const val LUCK_AMOUNT = 1
         val IMG = DefaultMod.makeCardPath("Power.png")
         private val RARITY = CardRarity.COMMON
         private val TARGET = CardTarget.SELF
