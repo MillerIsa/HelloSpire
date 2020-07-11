a28dc8faa93bae6607db5db935bd0a70ab374a63
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-07-11 16:34:32 -0700

    Refactors AbsractStealACardByType from its Java version to its Kotlin version.

---
+++ src/main/java/robTheSpire/cards/AbstractStealACardByType.kt
@@ -0,0 +1,37 @@
+package robTheSpire.cards
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+import robTheSpire.DefaultMod.Companion.makeCardPath
+import robTheSpire.actions.StealCardOfTypeAction
+import robTheSpire.characters.TheDefault.Enums.COLOR_GRAY
+
+abstract class AbstractStealACardByType @JvmOverloads constructor(theType: CardType, ID: String?, cardStrings: CardStrings, rarity: CardRarity? = CardRarity.COMMON) : AbstractDefaultCard(ID, cardStrings.NAME, makeCardPath("StealACard.png"), 2, cardStrings.DESCRIPTION, CardType.SKILL, COLOR_GRAY, rarity, CardTarget.SELF) {
+    private val cardStrings: CardStrings
+    override fun use(p: AbstractPlayer, m: AbstractMonster?) {
+        addToBot(StealCardOfTypeAction(this.type))
+    }
+
+    override fun upgrade() {
+        if (!upgraded) {
+            //this.magicNumber = this.baseMagicNumber = 2;
+            upgradeBaseCost(1)
+            //            this.exhaust = false;
+//            ExhaustiveVariable.setBaseValue(this, 2);
+//            ExhaustiveField.ExhaustiveFields.isExhaustiveUpgraded.set(this, true);
+            upgradeName()
+            rawDescription = cardStrings.UPGRADE_DESCRIPTION
+            initializeDescription()
+        }
+    }
+
+    //Extending class MUST call this constructor
+    init {
+        //ExhaustiveVariable.setBaseValue(this,2);
+        //this.magicNumber = this.baseMagicNumber = 1;
+        exhaust = true
+        this.type = theType
+        this.cardStrings = cardStrings
+    }
+}
