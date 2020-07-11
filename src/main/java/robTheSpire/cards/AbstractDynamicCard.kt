e71b486dd50062303d275d9d46f80cd537422067
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-07-11 16:18:18 -0700

    Refactors AbstractDynamicCard from java to its Kotlin version.

---
+++ src/main/java/robTheSpire/cards/AbstractDynamicCard.kt
@@ -0,0 +1,19 @@
+package robTheSpire.cards
+
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import robTheSpire.cards.AbstractDefaultCard
+
+abstract class AbstractDynamicCard  // "How come DefaultCommonAttack extends CustomCard and not DynamicCard like all the rest?"
+// Well every card, at the end of the day, extends CustomCard.
+// Abstract Default Card extends CustomCard and builds up on it, adding a second magic number. Your card can extend it and
+// bam - you can have a second magic number in that card (Learn Java inheritance if you want to know how that works).
+// Abstract Dynamic Card builds up on Abstract Default Card even more and makes it so that you don't need to add
+// the NAME and the DESCRIPTION into your card - it'll get it automatically. Of course, this functionality could have easily
+// Been added to the default card rather than creating a new Dynamic one, but was done so to deliberately.
+(id: String?,
+ img: String?,
+ cost: Int,
+ type: CardType?,
+ color: CardColor?,
+ rarity: CardRarity?,
+ target: CardTarget?) : AbstractDefaultCard(id, CardCrawlGame.languagePack.getCardStrings(id).NAME, img, cost, CardCrawlGame.languagePack.getCardStrings(id).DESCRIPTION, type, color, rarity, target)
