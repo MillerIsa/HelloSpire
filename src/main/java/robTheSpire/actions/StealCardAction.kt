46aa194a4a5ae3a0cd46912410769b196f851fe6
IsaiahMiller <miller.isaiah.n@gmail.com>
2020-07-11 18:24:21 -0700

    Bulk refactors the remaining Actions from their Java versions to their Kotlin versions. Also removes card filter interface to convert to Kotlin's style of writing Lambda expressions.

--- src/main/java/robTheSpire/actions/StealCardAction.kt
+++ src/main/java/robTheSpire/actions/StealCardAction.kt
@@ -1,34 +1,31 @@
-package robTheSpire.actions;
+package robTheSpire.actions
 
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.random.Random;
-import robTheSpire.characters.TheDefault;
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.cards.AbstractCard.CardRarity
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.random.Random
+import robTheSpire.characters.TheDefault.Enums.COLOR_GRAY
 
-public class StealCardAction extends AbstractCardStealingAction {
-    private static final int RARITY_FLAT_PERCENTAGE_BUFF = -25;
-    public StealCardAction() {
-        super(null,(c, prohibited) -> c.color != AbstractCard.CardColor.COLORLESS && c.type != AbstractCard.CardType.CURSE && c.type != AbstractCard.CardType.STATUS && c.color == TheDefault.Enums.getCOLOR_GRAY() && !prohibited.contains(c.cardID));
+class StealCardAction : AbstractCardStealingAction(null, { c: AbstractCard, prohibited: Set<String?>? -> c.color != AbstractCard.CardColor.COLORLESS && c.type != AbstractCard.CardType.CURSE && c.type != AbstractCard.CardType.STATUS && c.color == COLOR_GRAY && !prohibited?.contains(c.cardID)!! }) {
+    override fun rollRarity(rng: Random?): CardRarity {
+        var roll = AbstractDungeon.cardRng.random(99)
+        roll += AbstractDungeon.cardBlizzRandomizer
+        roll += RARITY_FLAT_PERCENTAGE_BUFF
+        println("Rolled a $roll")
+        return if (AbstractDungeon.currMapNode == null) getCardRarityFallback(roll) else AbstractDungeon.getCurrRoom().getCardRarity(roll)
     }
 
-
-    @Override
-    protected AbstractCard.CardRarity rollRarity(Random rng) {
-        int roll = AbstractDungeon.cardRng.random(99);
-        roll += AbstractDungeon.cardBlizzRandomizer;
-        roll += RARITY_FLAT_PERCENTAGE_BUFF;
-        System.out.println("Rolled a " + roll);
-        return AbstractDungeon.currMapNode == null ? getCardRarityFallback(roll) : AbstractDungeon.getCurrRoom().getCardRarity(roll);
-    }
-
-    @Override
-    protected AbstractCard.CardRarity getCardRarityFallback(int roll) {
-        System.out.println("WARNING: Current map node was NULL while rolling rarities. Using fallback card rarity method.");
-        int rareRate = 3 + RARITY_FLAT_PERCENTAGE_BUFF;
-        if (roll < rareRate) {
-            return AbstractCard.CardRarity.RARE;
+    override fun getCardRarityFallback(roll: Int): CardRarity {
+        println("WARNING: Current map node was NULL while rolling rarities. Using fallback card rarity method.")
+        val rareRate = 3 + RARITY_FLAT_PERCENTAGE_BUFF
+        return if (roll < rareRate) {
+            CardRarity.RARE
         } else {
-            return roll < 40 ? AbstractCard.CardRarity.UNCOMMON : AbstractCard.CardRarity.COMMON;
+            if (roll < 40) CardRarity.UNCOMMON else CardRarity.COMMON
         }
+    }
+
+    companion object {
+        private const val RARITY_FLAT_PERCENTAGE_BUFF = -25
     }
 }
